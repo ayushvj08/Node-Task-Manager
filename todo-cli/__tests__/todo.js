29fb0050f9a26e260317a6ce2a07d57e83899d67
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-03-09 16:28:54 +0530

    Add overdue, dueToday & dueLater tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, add, markAsComplete } = todoList();
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
 describe("Todo Test Suite", () => {
   beforeAll(() => {
@@ -17,7 +17,7 @@ describe("Todo Test Suite", () => {
     const todosCount = all.length;
     add({
       title: "Adding a New Todo Item",
-      dueDate: new Date(),
+      dueDate: new Date().toISOString().split("T")[0],
       completed: false,
     });
     expect(all.length).toBe(todosCount + 1);
@@ -27,5 +27,44 @@ describe("Todo Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  // 3.  Retrieval of overdue todos
+  test("Retrieval of overdue todos", () => {
+    const overdueTodosCount = overdue().length;
+    const oneday = 24 * 60 * 60 * 1000;
+
+    add({
+      title: "Add an overdue todo item",
+      dueDate: new Date(Number(new Date()) - oneday)
+        .toISOString()
+        .split("T")[0],
+      completed: false,
+    });
+    expect(overdue().length).toBe(overdueTodosCount + 1);
+  });
+  // 4.  Retrieval of due today todos
+  test("Retrieval of due today todos", () => {
+    const dueTodayTodosCount = dueToday().length;
+    add({
+      title: "Add an overdue todo item",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+    expect(dueToday().length).toBe(dueTodayTodosCount + 1);
+  });
+  // 5.  Retrieval of due later todos
+  test("Retrieval of due later todos", () => {
+    const dueLaterTodosCount = dueLater().length;
+    const oneday = 24 * 60 * 60 * 1000;
+
+    add({
+      title: "Add an overdue todo item",
+      dueDate: new Date(Number(new Date()) + oneday)
+        .toISOString()
+        .split("T")[0],
+      completed: false,
+    });
+    console.log(dueLater().length);
+    expect(dueLater().length).toBe(dueLaterTodosCount + 1);
   });
 });
